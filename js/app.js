a8cef1d13537b6a4a35d9ed28f00447b42ef1dd9
tcasey <trevort.casey@gmail.com>
2015-09-23 13:28:53 -0600

    FINAL commit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-	function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
-    }
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  		   .mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-=======
 	doIntro();
 	var isKeydown = false
 	function playHadouken() {
@@ -36,7 +22,6 @@ $(document).ready(function() {
     	$('.instructions').hide();
 		$('.ryu-still').show();
 		$('.logo').show();
->>>>>>> master
   })
    .mousedown(function() {
     console.log('mousedown');
@@ -49,38 +34,12 @@ $(document).ready(function() {
   {'left': '1020px'},
   500,
   function() {
-<<<<<<< HEAD
-    $(this).hide();
-    $(this).css('left', '520px');
-=======
     	$(this).hide();
     	$(this).css('left', '520px');
->>>>>>> master
   });
   })
   .mouseup(function() {
     console.log('mouseup');
-<<<<<<< HEAD
-    $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
-  })
-  	.keydown(function(event) {
-    if (event.which == 88) {
-    console.log('keydown');
-     $('.ryu-cool').show();
-     $('.ryu-still').hide();
-   };
-   })
-  	.keyup(function(event) {
-    if (event.which == 88) {
-    console.log('keyup');	
-     $('.ryu-cool').hide();
-     $('.ryu-still').show();
-   };
-   })
-
-  });
-=======
     	$('.ryu-action').hide();
   		$('.ryu-ready').show();
   });
@@ -106,5 +65,4 @@ $(document).keydown(function(event) {
    })
 
   });
->>>>>>> master
 
