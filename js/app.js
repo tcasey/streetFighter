d2e028efd18d80be5ab41f41dfffeeeb0233605c
tcasey <trevort.casey@gmail.com>
2015-09-23 12:43:34 -0600

    final commit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,57 @@
 $(document).ready(function() {
+	doIntro();
 	var isKeydown = false
 	function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].volume = 0.1;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play();
     }
-  $('.ryu').mouseenter(function() {
-    $('.ryu-action').hide();
-    $('.ryu-ready').show();
+    function doIntro() {
+  		$('#theme-song')[0].volume = 0.2;
+  		$('#theme-song')[0].load();
+  		$('#theme-song')[0].play();
+}
+  	$('.ryu').mouseenter(function() {
+   		$('.ryu-action').hide();
+   		$('.logo').hide();
+   		$('.ryu-ready').show();
+		$('.instructions').show();
   })
   		   .mouseleave(function() {
-    $('.ryu-action').hide();
-	$('.ryu-still').show();
+    	$('.ryu-action').hide();
+    	$('.instructions').hide();
+		$('.ryu-still').show();
+		$('.logo').show();
   })
    .mousedown(function() {
     console.log('mousedown');
     playHadouken();
-    $('.ryu-action').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
+    	$('.ryu-action').hide();
+    	$('.instructions').hide();
+    	$('.logo').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').finish().show().animate(
   {'left': '1020px'},
   500,
   function() {
-    $(this).hide();
-    $(this).css('left', '520px');
+    	$(this).hide();
+    	$(this).css('left', '520px');
   });
   })
   .mouseup(function() {
     console.log('mouseup');
-    $('.ryu-action').hide();
-  	$('.ryu-ready').show();
+    	$('.ryu-action').hide();
+  		$('.ryu-ready').show();
   });
 $(document).keydown(function(event) {
 	
-    if (!isKeydown && event.which == 88) {
+    if (!isKeydown && event.which == 88) {	
     console.log('keydown');
-     $('.ryu-action').hide();
-     $('.ryu-cool').show();
+    	$('.ryu-action').hide();
+    	$('.instructions').hide();
+     	$('.ryu-cool').show();
+     	$('.logo').show();
+		$('.ryu-cool').show();
 		isKeydown = true
    };
    })
@@ -44,8 +59,8 @@ $(document).keydown(function(event) {
   		isKeydown = false
     if (event.which == 88) {
     console.log('keyup');	
-     $('.ryu-action').hide();
-     $('.ryu-still').show();
+     	$('.ryu-action').hide();
+     	$('.ryu-still').show();
    };
    })
 
