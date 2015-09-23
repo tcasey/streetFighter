3a6db8d1a3f02b3ea2cd01e8159c07505bf821ca
tcasey <trevort.casey@gmail.com>
2015-09-22 21:13:16 -0600

    .keydown needs work

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
+	function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-  .mouseleave(function() {
+  		   .mouseleave(function() {
     $('.ryu-ready').hide();
 	$('.ryu-still').show();
   })
-  .mousedown(function() {
+   .mousedown(function() {
     console.log('mousedown');
     playHadouken();
     $('.ryu-ready').hide();
@@ -18,17 +23,27 @@ $(document).ready(function() {
   function() {
     $(this).hide();
     $(this).css('left', '520px');
-  }
-);
+  });
   })
   .mouseup(function() {
     console.log('mouseup');
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show();
+  })
+  	.keydown(function(event) {
+    if (event.which == 88) {
+    console.log('keydown');
+     $('.ryu-cool').show();
+     $('.ryu-still').hide();
+   };
+   })
+  	.keyup(function(event) {
+    if (event.which == 88) {
+    console.log('keyup');	
+     $('.ryu-cool').hide();
+     $('.ryu-still').show();
+   };
+   })
+
   });
-});
-function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
-    }
+
